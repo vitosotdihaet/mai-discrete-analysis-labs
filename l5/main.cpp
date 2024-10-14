4e28017ef5c0324210a89967cb6057653801f6b3
vitosotdihaet <vitostikitos@gmail.com>
2024-10-14 19:55:20 +0300

    [l5] need to debug this shaet

--- l5/main.cpp
+++ l5/main.cpp
@@ -30,47 +30,74 @@
 
 
 
+
+
+// oop is a fucking joke
 class SuffixTreeNode;
+class SuffixTreeEdge;
 
-class SuffixTreeEdge {
-public:
-    int start = 0;
-    std::shared_ptr<int> end = nullptr;
-    SuffixTreeNode *node;
 
-    SuffixTreeEdge(const int &start, const std::shared_ptr<int> &end) : start(start), end(end) {}
 
-    friend std::ostream& operator<<(std::ostream &os, const SuffixTreeEdge &edge) {
-        os << "s: " << edge.start << " e: " << *edge.end << " (" << edge.end << ')';
-        return os;
-    }
-};
 
 
 class SuffixTreeNode {
 public:
     // chars to edges
-    std::map<char, SuffixTreeEdge*> next;
+    std::map<char, std::shared_ptr<SuffixTreeEdge>> next;
     SuffixTreeNode *suffixLink = nullptr;
 
-    SuffixTreeNode(const char &value, const int &start, const std::shared_ptr<int> &end) {
-        next[value] = new SuffixTreeEdge(start, end);
-    }
+    SuffixTreeNode() {};
+    SuffixTreeNode(const char &value, const int &start, const std::shared_ptr<int> &end);
 
-    friend std::ostream& operator<<(std::ostream &os, const SuffixTreeNode &node) {
-        os << "next:\n";
-        for (auto [key, value] : node.next) {
-            os << "\t" << key << " --- " << *value << '\n';
-        }
-        os << "suffixLink: " << node.suffixLink;
-        return os;
-    }
+    friend std::ostream& operator<<(std::ostream &os, const SuffixTreeNode &node);
 };
+
+
+class SuffixTreeEdge {
+public:
+    int start = 0;
+    std::shared_ptr<int> end = nullptr;
+
+    std::shared_ptr<SuffixTreeNode> node = nullptr;
+
+    SuffixTreeEdge(const int &start, const std::shared_ptr<int> &end);
+
+    friend std::ostream& operator<<(std::ostream &os, const SuffixTreeEdge &edge);
+};
+
+
+
+
+
+SuffixTreeEdge::SuffixTreeEdge(const int &start, const std::shared_ptr<int> &end) : start(start), end(end), node(std::make_shared<SuffixTreeNode>()) {}
+
+std::ostream& operator<<(std::ostream &os, const SuffixTreeEdge &edge) {
+    os << "s: " << edge.start << " e: " << *edge.end << " (" << edge.end << ") node: " << edge.node;
+    return os;
+}
+
+
+
+SuffixTreeNode::SuffixTreeNode(const char &value, const int &start, const std::shared_ptr<int> &end) {
+    next[value] = std::make_shared<SuffixTreeEdge>(start, end);
+}
+
+std::ostream& operator<<(std::ostream &os, const SuffixTreeNode &node) {
+    os << "next:\n";
+    for (const auto& [key, value] : node.next) {
+        os << "\t" << key << " --- " << *value << '\n';
+    }
+    os << "suffixLink: " << node.suffixLink;
+    return os;
+}
+
+
+
 
 
 class SuffixTree {
 private:
-    SuffixTreeNode *root = nullptr;
+    std::shared_ptr<SuffixTreeNode> root = nullptr;
 
 public:
     SuffixTree(std::string inputString) {
@@ -79,32 +106,48 @@ public:
 
 
         std::shared_ptr<int> end = std::make_shared<int>(1);
-        this->root = new SuffixTreeNode(inputString[0], 0, end);
+        this->root = std::make_shared<SuffixTreeNode>(inputString[0], 0, end);
 
-        SuffixTreeNode *activeNode = this->root;
-        SuffixTreeEdge *activeEdge = nullptr;
-        int activeLength = 0;
+        std::shared_ptr<SuffixTreeNode> activeNode = this->root;
+        std::shared_ptr<SuffixTreeEdge> activeEdge = nullptr;
+        size_t activeLength = 0;
 
-        int remainder = 0;
+        size_t remainder = 0;
 
 
         for (size_t phase = 1; phase < inputStringLength; ++phase) {
             std::cout << "------------------------ PHASE " << phase << " ------------------------\n";
-            std::cout << "ROOT:\n" << *root << '\n';
-        }
-    }
+            std::cout << "Active Point: node: " << activeNode << " edge: " << activeEdge << " length: " << activeLength << '\n';
+            std::cout << "Remainder: " << remainder << '\n';
+            std::cout << "root:\n" << *root << '\n';
 
-    ~SuffixTree() {
+            ++*end; // postfix ++ is not implemented (???)
+
+            for (size_t currentCharIndex = phase - remainder; currentCharIndex <= phase; ++currentCharIndex) {
+                char currentChar = inputString[currentCharIndex];
+                auto entry = activeNode->next.find(currentChar);
+                if (entry != activeNode->next.end()) { // char is present
+                    activeEdge = entry->second;
+                    activeLength++;
+                    remainder++;
+                } else { // char is not present
+                    activeNode->next[currentChar] = std::make_shared<SuffixTreeEdge>(phase, end);
+                    remainder--;
+                }
+            }
+        }
     }
 };
 
 
-int main() {
-    std::string s1("lol"), s2("alob");
 
+
+
+int main() {
+    // std::string s1("lol"), s2("alob");
     // std::cin >> s1 >> s2;
 
-    SuffixTree st(s1 + '#' + s2);
+    SuffixTree st("abcabxabcd");
 
     return 0;
 }
