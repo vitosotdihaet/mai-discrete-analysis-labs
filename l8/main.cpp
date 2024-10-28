13c4ac6dbe69dc5b9e5d8b8d84209565992dec43
vitosotdihaet <vitostikitos@gmail.com>
2024-10-28 16:09:21 +0300

    [l8] add DP realisation optimized for time and storage

--- l8/main.cpp
+++ l8/main.cpp
@@ -5,7 +5,7 @@
 
 
 template <typename T>
-T power(T base, T exponent) {
+T power(const T base, const T exponent) {
     T result = 1;
     for (T _ = 1; _ < exponent; ++_) {
         result *= base;
@@ -15,7 +15,7 @@ T power(T base, T exponent) {
 
 
 
-std::vector<uint32_t> dynamic(const std::vector<uint64_t> &coins, const uint32_t amount) {
+std::vector<uint32_t> dynamicOptimizedForTime(const std::vector<uint64_t> &coins, const uint32_t amount) {
     const size_t coinsSize = coins.size();
 
     // vector of pairs: count of coins and the last added coin for current amount
@@ -49,11 +49,48 @@ std::vector<uint32_t> dynamic(const std::vector<uint64_t> &coins, const uint32_t
 }
 
 
-std::vector<uint64_t> greedy(const std::vector<uint64_t> &coins, uint64_t amount) {
+std::vector<uint32_t> dynamicOptimizedForStorage(const std::vector<uint64_t> &coins, const uint32_t amount) {
+    const size_t coinsSize = coins.size();
+
+    // vector of count of coins for current amount
+    std::vector<uint32_t> dp(amount + 1, amount);
+    dp[0] = 0;
+
+    for (uint32_t currentAmount = 0; currentAmount <= amount; ++currentAmount) {
+        for (size_t currentCoinIndex = 0; currentCoinIndex < coinsSize; ++currentCoinIndex) {
+            const uint64_t currentCoin = coins[currentCoinIndex];
+
+            if (currentAmount + currentCoin > amount) break;
+
+            dp[currentAmount + currentCoin] = std::min(dp[currentAmount] + 1, dp[currentAmount + currentCoin]);
+        }
+    }
+
+    std::vector<uint32_t> result(coinsSize, 0);
+
+    size_t currentAmount = amount;
+    while (currentAmount != 0) {
+        for (size_t currentCoinIndex = 0; currentCoinIndex < coinsSize; ++currentCoinIndex) {
+            const uint64_t currentCoin = coins[currentCoinIndex];
+
+            if (currentAmount < currentCoin) break;
+
+            if (dp[currentAmount - currentCoin] + 1 == dp[currentAmount]) {
+                currentAmount -= currentCoin;
+                result[currentCoinIndex]++;
+            }
+        }
+    }
+
+    return result;
+}
+
+
+std::vector<uint32_t> greedy(const std::vector<uint64_t> &coins, uint32_t amount) {
     const size_t coinsSize = coins.size();
     size_t currentCoinIndex = coinsSize - 1;
 
-    std::vector<uint64_t> result(coinsSize, 0);
+    std::vector<uint32_t> result(coinsSize, 0);
 
     while (amount != 0) {
         uint64_t currentCoin = coins[currentCoinIndex];
@@ -82,8 +119,9 @@ int main() {
         coins[currentExponent - 1] = power(base, currentExponent);
     }
 
-    std::vector<uint32_t> result = dynamic(coins, amount);
-    // std::vector<uint64_t> result = greedy(coins, amount);
+    std::vector<uint32_t> result = dynamicOptimizedForTime(coins, amount);
+    // std::vector<uint32_t> result = dynamicOptimizedForStorage(coins, amount);
+    // std::vector<uint32_t> result = greedy(coins, amount);
 
     for (uint32_t count : result) {
         std::cout << count << '\n';
