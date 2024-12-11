94ed7cd4ed43d621ee9975033323c63879cf3781
vitosotdihaet <vitostikitos@gmail.com>
2024-12-12 02:21:16 +0300

    [cp] bruuuh

--- cp/main.cpp
+++ cp/main.cpp
@@ -1,73 +1,204 @@
 #include <iostream>
+#include <iomanip>
+#include <optional>
+
 #include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+#include <cmath>
+#include <memory>
 
 
 
 template <typename T>
-class Edge {
-private:
-    T from = 0, to = 0;
-
-public:
-    Edge() {}
-
-    Edge(T x, T y) : from(x), to(y) {}
-
-    friend std::istream& operator>>(std::istream &in, Edge<T> &edge) {
-        in >> edge.from >> edge.to;
-        return in;
-    }
-};
-
-
-template <typename T>
-class Point {
+class Node {
 private:
     T x = 0, y = 0;
-
 public:
-    Point() {}
+    size_t id = 0;
+    static size_t globalID;
+    double heuristicCost = std::numeric_limits<double>::infinity();
+    double pathCost = std::numeric_limits<double>::infinity();
 
-    Point(T x, T y) : x(x), y(y) {}
+    std::set<std::pair<size_t, double>> edges;
 
-    friend std::istream& operator>>(std::istream &in, Point<T> &point) {
+private:
+public:
+    Node() : id(Node<T>::globalID++) {}
+
+    Node(T x, T y) : x(x), y(y), id(Node<T>::globalID++) {}
+
+    double to(Node<T> other) {
+        return sqrt((this->x - other.x) * (this->x - other.x) + (this->y - other.y) * (this->y - other.y));
+    }
+
+    friend std::istream& operator>>(std::istream &in, Node<T> &point) {
         in >> point.x >> point.y;
         return in;
     }
 };
 
+template <typename T>
+size_t Node<T>::globalID = 0;
 
+
+template <typename T>
+class AStar {
+private:
+    std::vector<Node<T>> nodes;
+
+    struct Compare {
+        const std::vector<Node<T>>& nodes;
+
+        Compare(const std::vector<Node<T>>& nodes) : nodes(nodes) {}
+
+        bool operator()(size_t firstIndex, size_t secondIndex) const {
+            const auto& first = this->nodes[firstIndex];
+            const auto& second = this->nodes[secondIndex];
+            // std::cout << "compare " << first.id << ' ' << second.id << '\n';
+            // std::cout << (first.pathCost + first.heuristicCost) << " vs " << (second.pathCost + second.heuristicCost) << ' ' << ((first.pathCost + first.heuristicCost) > (second.pathCost + second.heuristicCost)) << '\n';
+            return (first.pathCost + first.heuristicCost) > (second.pathCost + second.heuristicCost);
+        }
+    };
+
+    Compare compare;
+
+public:
+    AStar(std::vector<Node<T>> &nodes) : nodes(nodes), compare(nodes) {}
+
+    std::optional<double> shortestPath(size_t fromIndex, size_t toIndex) {
+        if (fromIndex == toIndex) { return 0; }
+
+        Node<T> &start = this->nodes[fromIndex];
+        Node<T> &end = this->nodes[toIndex]; 
+
+        for (Node<T> &n : nodes) {
+            n.pathCost = std::numeric_limits<double>::infinity();
+            n.heuristicCost = n.to(end);
+        }
+
+        start.pathCost = 0;
+        start.heuristicCost = start.to(end);
+
+        std::multiset<size_t, Compare> open(compare);
+        open.insert(start.id);
+
+        size_t currentID = 0;
+
+        // all node ids that were visited
+        std::set<size_t> visited;
+
+        // child id -> parent id
+        std::map<size_t, size_t> parents;
+
+        while (!open.empty()) {
+            // get node with lowest path cost + heuristic cost
+            currentID = *open.begin();
+            open.erase(open.begin());
+
+            if (currentID == toIndex) {
+                break;
+            }
+
+            visited.insert(currentID);
+
+            const Node<T> &current = this->nodes[currentID];
+
+            // iterate over all edges
+            for (const auto &[nextID, edgeLength] : current.edges) {
+                // skip a node at this edge if it was visited before
+                if (visited.count(nextID)) { continue; }
+
+                Node<T> &next = this->nodes[nextID];
+
+                double newCost = current.pathCost + edgeLength;
+
+                if (newCost < next.pathCost) {
+                    next.pathCost = current.pathCost + edgeLength;
+                    parents[nextID] = currentID;
+
+                    for (auto it = open.begin(); it != open.end(); ++it) {
+                        if (*it == nextID) {
+                            open.erase(it);
+                            break;
+                        }
+                    }
+
+                    open.insert(nextID);
+                }
+            }
+        }
+
+        if (currentID != toIndex) {
+            return std::nullopt;
+        }
+
+        double length = 0;
+        auto it = parents.find(currentID);
+        while (it != parents.end()) {
+            size_t parentID = it->second;
+            Node<T> &parent = this->nodes[parentID];
+
+            length += parent.to(this->nodes[currentID]);
+            // std::cout << parent.id << '\n';
+
+            currentID = parent.id;
+            it = parents.find(currentID);
+        }
+
+        return length;
+    }
+};
+
+
+#include "test.hpp"
 
 int main() {
-    uint16_t vertexCount, edgeCount;
-    std::cin >> vertexCount >> edgeCount;
+#ifdef TEST
+    test();
+    return 0;
+#endif
+    uint32_t nodeCount, edgeCount;
+    std::cin >> nodeCount >> edgeCount;
 
-    std::vector<Point<int32_t>> verticies;
-    verticies.reserve(vertexCount);
+    std::vector<Node<double>> nodes;
+    nodes.reserve(nodeCount);
 
-    for (uint16_t i = 0; i < vertexCount; ++i) {
-        int32_t x, y;
+    for (uint32_t i = 0; i < nodeCount; ++i) {
+        double x, y;
         std::cin >> x >> y;
-        verticies.emplace_back(x, y);
+        nodes.emplace_back(x, y);
     }
 
-    std::vector<Edge<uint16_t>> edges;
-    edges.reserve(edgeCount);
-
-    for (uint16_t i = 0; i < edgeCount; ++i) {
-        uint16_t from, to;
+    for (uint32_t i = 0; i < edgeCount; ++i) {
+        size_t from, to;
         std::cin >> from >> to;
-        edges.emplace_back(from, to);
+        from--; to--;
+
+        double distance = nodes[from].to(nodes[to]);
+        nodes[from].edges.emplace(to, distance);
+        nodes[to].edges.emplace(from, distance);
     }
 
 
-    uint16_t requestCount;
+    AStar astar(nodes);
+
+    uint32_t requestCount;
     std::cin >> requestCount;
 
-    for (uint16_t i = 0; i < requestCount; ++i) {
-        Edge<int16_t> request;
-        std::cin >> request;
+    for (uint32_t i = 0; i < requestCount; ++i) {
+        uint32_t from, to;
+        std::cin >> from >> to;
+        from--; to--;
 
-        //* process the request...
+        std::optional<double> result = astar.shortestPath(from, to);
+        if (result.has_value()) {
+            std::cout << std::fixed << std::setprecision(6) << *result << '\n';
+        } else {
+            std::cout << -1.0 << '\n';
+        }
     }
 }
---
+++ cp/test.hpp
@@ -0,0 +1,409 @@
+#define TEST
+#include <iostream>
+#include <iomanip>
+#include <optional>
+
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#include <cmath>
+#include <memory>
+
+
+template <typename T>
+bool test_case(
+    std::vector<std::pair<T, T>> &positions,
+    std::vector<std::pair<size_t, size_t>> &edges,
+    std::vector<std::pair<size_t, size_t>> &fromtos,
+    std::vector<double> &answers
+) {
+    Node<T>::globalID = 0;
+    size_t nodeCount = positions.size();
+    std::vector<Node<T>> nodes;
+    nodes.reserve(nodeCount);
+
+    for (const auto &[x, y] : positions) {
+        nodes.emplace_back(x, y);
+    }
+
+    for (auto &[from, to] : edges) {
+        from--; to--;
+        double distance = nodes[from].to(nodes[to]);
+        nodes[from].edges.emplace(to, distance);
+        nodes[to].edges.emplace(from, distance);
+    }
+    
+    AStar astar(nodes);
+
+    for (size_t i = 0; i < fromtos.size(); ++i) {
+        std::optional<double> result = astar.shortestPath(fromtos[i].first - 1, fromtos[i].second - 1);
+        double answer = answers[i];
+        if (!((result.has_value() && std::abs(*result - answer) < 1e-9) || (!result.has_value() && answer == -1.0))) {
+            std::cout << i << ": result = " << result.value_or(-1) << " and answer is " << answer << '\n';
+            return false;
+        }
+    }
+
+    return true;
+}
+
+typedef double T;
+
+void test() {
+    {
+        std::vector<std::pair<T, T>> positions = {
+            { 0,  0},
+            { 1,  1},
+            {-1,  1},
+            { 0,  2},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 2},
+            {1, 3},
+            {2, 4},
+            {3, 4},
+            {1, 4},
+        };
+
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 4},
+        };
+
+        std::vector<double> answers = {
+            2,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+    
+    {
+        std::vector<std::pair<T, T>> positions = {
+            { 0,  0},
+            { 2,  1},
+            { 1, -1},
+            { 2, -2},
+            { 4, -2},
+            { 4,  2},
+            {-1,  2},
+            { 2,  3},
+            { 3, -1},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 2},
+            {1, 3},
+            {1, 4},
+            {1, 9},
+            {5, 8},
+            {9, 6},
+            {7, 6},
+            {6, 7},
+            {6, 9},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 6},
+            {5, 8},
+            {1, 5},
+            {7, 9},
+        };
+        std::vector<double> answers = {
+            sqrt(10) + sqrt(10),
+            sqrt(29),
+            -1.0,
+            5 + sqrt(10),
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            {-1, 0},
+            {-1, 1},
+            {-1, 2},
+            {0, 0},
+            {0, 1},
+            {0, 2},
+            {1, 0},
+            {1, 1},
+            {1, 2},
+            {2, 0},
+            {3, 0},
+            {3, 1},
+            {3, 2},
+            {4, 0},
+            {4, 1},
+            {4, 2},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 4},
+            {1, 5},
+            {1, 2},
+            {2, 4},
+            {2, 5},
+            {2, 6},
+            {2, 3},
+            {3, 5},
+            {3, 6},
+            
+            {4, 7},
+            {4, 8},
+            {4, 5},
+            {5, 7},
+            {5, 8},
+            {5, 9},
+            {5, 6},
+            {6, 8},
+            {6, 9},
+
+            {7, 10},
+            {7, 8},
+            {8, 10},
+            {8, 9},
+
+            {10, 11},
+            {10, 12},
+
+            {11, 14},
+            {11, 15},
+            {11, 12},
+            {12, 14},
+            {12, 15},
+            {12, 16},
+            {12, 13},
+            {13, 15},
+            {13, 16},
+
+            {14, 15},
+            {15, 16},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {4, 16},
+            {1, 16},
+            {14, 16},
+            {8, 13},
+            {13, 8},
+        };
+        std::vector<double> answers = {
+            1 + 1 + sqrt(2) + sqrt(2),
+            1 + 1 + 1 + sqrt(2) + sqrt(2),
+            2,
+            sqrt(2) + sqrt(2) + 1,
+            sqrt(2) + sqrt(2) + 1,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            { 2,  1},
+            { 1, -1},
+            { 3, -1},
+            { 2, -1},
+            { 3,  1},
+            { 3,  2},
+            { 1,  2},
+            { 1,  1},
+            {-1,  1},
+            {-1, -1},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 2},
+            {2, 3},
+            {3, 4},
+            {4, 5},
+            {5, 6},
+            {7, 8},
+            {9, 7},
+            {9, 8},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 6},
+            {1, 7},
+            {5, 9},
+            {10, 9},
+            {10, 3},
+            {10, 10},
+            {8, 7},
+        };
+        std::vector<double> answers = {
+            sqrt(5) + 2 + 1 + sqrt(5) + 1,
+            -1,
+            -1,
+            -1,
+            -1,
+            0,
+            1,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            { 2,  2},
+            { 1, -1},
+            { 0,  1},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 2},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 2},
+            {2, 1},
+            {3, 3},
+            {1, 1},
+            {1, 3},
+            {2, 2},
+        };
+        std::vector<double> answers = {
+            sqrt(10),
+            sqrt(10),
+            0,
+            0,
+            -1,
+            0,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            { 2,  2},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 1}
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 1},
+        };
+        std::vector<double> answers = {
+            0,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            {-1, 0},
+            {-1, 1},
+            {-1, 2},
+            {0, 0},
+            {0, 1},
+            {0, 2},
+            {1, 0},
+            {1, 1},
+            {1, 2},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}, {9, 9},
+            {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9},
+            {1, 3}, {2, 4}, {3, 5}, {4, 6}, {5, 7}, {6, 8}, {7, 9},
+            {1, 4}, {2, 5}, {3, 6}, {4, 7}, {5, 8}, {6, 9},
+            {1, 5}, {2, 6}, {3, 7}, {4, 8}, {5, 9},
+            {1, 6}, {2, 7}, {3, 8}, {4, 9},
+            {1, 7}, {2, 8}, {3, 9}, 
+            {1, 8}, {2, 9},
+            {1, 9},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 2},
+            {2, 3},
+            {4, 1},
+            {1, 1},
+            {5, 9},
+            {2, 2},
+            {9, 1},
+        };
+        std::vector<double> answers = {
+            1,
+            1,
+            1,
+            0,
+            sqrt(2),
+            0,
+            sqrt(2) + sqrt(2),
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+
+    {
+        std::vector<std::pair<T, T>> positions = {
+            {1, 1},
+            {1, 2},
+            {2, 2},
+            {1, 4},
+            {2, 1},
+            {3, 0},
+            {4, 1},
+            {4, 2},
+            {3, 3},
+            {2, 3},
+        };
+        std::vector<std::pair<size_t, size_t>> edges = {
+            {1, 5},
+            {2, 1},
+            {3, 5},
+            {2, 3},
+            {4, 2},
+            {7, 8},
+            {5, 6},
+            {6, 7},
+            {10, 9},
+            {9, 8},
+            {2, 4},
+        };
+        std::vector<std::pair<size_t, size_t>> fromtos = {
+            {1, 10},
+            {3, 10},
+            {5, 10},
+            {8, 3},
+        };
+        std::vector<double> answers = {
+            1 + sqrt(2) + sqrt(2) + 1 + sqrt(2) + 1,
+            1 + sqrt(2) + sqrt(2) + 1 + sqrt(2) + 1,
+            sqrt(2) + sqrt(2) + 1 + sqrt(2) + 1,
+            1 + sqrt(2) + sqrt(2) + 1,
+        };
+
+        if (!test_case(positions, edges, fromtos, answers)) {
+            std::cout << "epic fail\n";
+        } else {
+            std::cout << "all gud\n";
+        }
+    }
+}
